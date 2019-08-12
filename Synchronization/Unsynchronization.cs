e8a0879f7c91a8145ed550ac97c2b22d22108f25
Константин Дьяченко <konard@me.com>
2019-08-13 01:33:28 +0300

    Another attempt to fix docs build.

--- Synchronization/Unsynchronization.cs
+++ Synchronization/Unsynchronization.cs
@@ -8,16 +8,16 @@ namespace Platform.Threading.Synchronization
     /// </summary>
     public class Unsynchronization : ISynchronization
     {
-        /// <include file='bin\Release\netstandard2.0\Documentation.xml' path='doc/members/member[@name="M:Platform.Threading.Synchronization.ISynchronization.ExecuteReadOperation(System.Action)"]/*'/>
+        /// <include file='bin\Release\netstandard2.0\Platform.Threading.xml' path='doc/members/member[@name="M:Platform.Threading.Synchronization.ISynchronization.ExecuteReadOperation(System.Action)"]/*'/>
         public void ExecuteReadOperation(Action action) => action();
 
-        /// <include file='bin\Release\netstandard2.0\Documentation.xml' path='doc/members/member[@name="M:Platform.Threading.Synchronization.ISynchronization.ExecuteReadOperation``1(System.Func{``0})"]/*'/>
+        /// <include file='bin\Release\netstandard2.0\Platform.Threading.xml' path='doc/members/member[@name="M:Platform.Threading.Synchronization.ISynchronization.ExecuteReadOperation``1(System.Func{``0})"]/*'/>
         public TResult ExecuteReadOperation<TResult>(Func<TResult> function) => function();
 
-        /// <include file='bin\Release\netstandard2.0\Documentation.xml' path='doc/members/member[@name="M:Platform.Threading.Synchronization.ISynchronization.ExecuteWriteOperation(System.Action)"]/*'/>
+        /// <include file='bin\Release\netstandard2.0\Platform.Threading.xml' path='doc/members/member[@name="M:Platform.Threading.Synchronization.ISynchronization.ExecuteWriteOperation(System.Action)"]/*'/>
         public void ExecuteWriteOperation(Action action) => action();
 
-        /// <include file='bin\Release\netstandard2.0\Documentation.xml' path='doc/members/member[@name="M:Platform.Threading.Synchronization.ISynchronization.ExecuteWriteOperation``1(System.Func{``0})"]/*'/>
+        /// <include file='bin\Release\netstandard2.0\Platform.Threading.xml' path='doc/members/member[@name="M:Platform.Threading.Synchronization.ISynchronization.ExecuteWriteOperation``1(System.Func{``0})"]/*'/>
         public TResult ExecuteWriteOperation<TResult>(Func<TResult> function) => function();
     }
 }
