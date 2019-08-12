ff656686ee079c310beb750a1732b09512328a94
Константин Дьяченко <konard@me.com>
2019-08-12 22:57:37 +0300

    Attempt to use <para> tags for splitting English and Russian versions of comments. And make a use of <typeparamref> tag for better refactoring in the future.

--- Synchronization/ISynchronized.cs
+++ Synchronization/ISynchronized.cs
@@ -1,35 +1,35 @@
 ﻿namespace Platform.Threading.Synchronization
 {
     /// <summary>
-    /// Represents extendable synchronized interface access gate.
-    /// Представляет расширяемый интерфейс шлюза синхронизированного доступа.
+    /// <para>Represents extendable synchronized interface access gate.</para>
+    /// <para>Представляет расширяемый интерфейс шлюза синхронизированного доступа.</para>
     /// </summary>
-    /// <typeparam name="TInterface">Synchronized interface. Синхронизируемый интерфейс.</typeparam>
+    /// <typeparam name="TInterface"><para>Synchronized interface.</para><para>Синхронизируемый интерфейс.</para></typeparam>
     public interface ISynchronized<out TInterface>
     {
         /// <summary>
-        /// Gets sychronization method.
-        /// Возвращает способ синхронизации.
+        /// <para>Gets sychronization method.</para>  
+        /// <para>Возвращает способ синхронизации.</para>
         /// </summary>
         ISynchronization SyncRoot { get; }
 
         /// <summary>
-        /// Get source version of TInterface, that does not garantee thread safe access synchronization.
-        /// Возвращает исходную версию TInterface, которая не гарантирует потокобезопасную синхронизацию доступа.
+        /// <para>Get source version of <typeparamref name="TInterface"/>, that does not garantee thread safe access synchronization.</para>
+        /// <para>Возвращает исходную версию <typeparamref name="TInterface"/>, которая не гарантирует потокобезопасную синхронизацию доступа.</para>
         /// </summary>
         /// <remarks>
-        /// It is unsafe to use it directly, unless compound context using SyncRoot is created.
-        /// Использовать напрямую небезопасно, за исключением ситуации когда создаётся составной контекст с использованием SyncRoot.
+        /// <para>It is unsafe to use it directly, unless compound context using SyncRoot is created.</para>
+        /// <para>Использовать напрямую небезопасно, за исключением ситуации когда создаётся составной контекст с использованием SyncRoot.</para>
         /// </remarks>
         TInterface Unsync { get; }
 
         /// <summary>
-        /// Get wrapped/decorated version of TInterface, that does garantee thread safe access synchronization.
-        /// Возвращает обернутую/декорированную версию TInterface, которая гарантирует потокобезопасную синхронизацию доступа.
+        /// <para>Get wrapped/decorated version of <typeparamref name="TInterface"/>, that does garantee thread safe access synchronization.</para>
+        /// <para>Возвращает обернутую/декорированную версию <typeparamref name="TInterface"/>, которая гарантирует потокобезопасную синхронизацию доступа.</para>
         /// </summary>
         /// <remarks>
-        /// It is safe to use it directly, because it must be thread safe implementation.
-        /// Безопасно использовать напрямую, так как реализация должна быть потокобезопасной.
+        /// <para>It is safe to use it directly, because it must be thread safe implementation.</para>
+        /// <para>Безопасно использовать напрямую, так как реализация должна быть потокобезопасной.</para>
         /// </remarks>
         TInterface Sync { get; }
     }
