3177cffd085c1759c8234389e8ccb37e877cd2c4
Константин Дьяченко <konard@me.com>
2019-08-12 22:55:52 +0300

    Added comments for ISynchronization

--- Synchronization/ISynchronization.cs
+++ Synchronization/ISynchronization.cs
@@ -2,11 +2,42 @@
 
 namespace Platform.Threading.Synchronization
 {
+    /// <summary>
+    /// <para>Represents a synchronization object that supports read and write operations.</para>
+    /// <para>Представляет объект синхронизации с поддержкой операций чтения и записи.</para>
+    /// </summary>
     public interface ISynchronization
     {
+        /// <summary>
+        /// <para>Executes action in read access mode.</para>
+        /// <para>Выполняет действие в режиме доступа для чтения.</para>
+        /// </summary>
+        /// <param name="action"><para>The action.</para><para>Действие.</para></param>
         void ExecuteReadOperation(Action action);
-        T ExecuteReadOperation<T>(Func<T> func);
+
+        /// <summary>
+        /// <para>Executes a function in read access mode and returns the function's result.</para>
+        /// <para>Выполняет функцию в режиме доступа для чтения и возвращает полученный из неё результат.</para>
+        /// </summary>
+        /// <typeparam name="TResult"><para>Type of function's result.</para><para>Тип результата функции.</para></typeparam>
+        /// <param name="function"><para>The function.</para><para>Функция.</para></param>
+        /// <returns><para>The function's result.</para><para>Результат функции.</para></returns>
+        TResult ExecuteReadOperation<TResult>(Func<TResult> function);
+
+        /// <summary>
+        /// <para>Executes action in write access mode.</para>
+        /// <para>Выполняет действие в режиме доступа для записи.</para>
+        /// </summary>
+        /// <param name="action"><para>The action.</para><para>Действие.</para></param>
         void ExecuteWriteOperation(Action action);
-        T ExecuteWriteOperation<T>(Func<T> func);
+
+        /// <summary>
+        /// <para>Executes a function in write access mode and returns the function's result.</para>
+        /// <para>Выполняет функцию в режиме доступа для записи и возвращает полученный из неё результат.</para>
+        /// </summary>
+        /// <typeparam name="TResult"><para>Type of function's result.</para><para>Тип результата функции.</para></typeparam>
+        /// <param name="function"><para>The function.</para><para>Функция.</para></param>
+        /// <returns><para>The function's result.</para><para>Результат функции.</para></returns>
+        TResult ExecuteWriteOperation<TResult>(Func<TResult> function);
     }
 }
