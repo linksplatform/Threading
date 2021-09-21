50936db2e41e3e5727d6fe76c317a651c434032e
uselessgoddess <dodickgod@gmail.com>
2021-09-21 20:13:27 +0300

    Add test

--- cpp/Platform.Threading.Tests/Sync.cpp
+++ cpp/Platform.Threading.Tests/Sync.cpp
@@ -1,6 +1,6 @@
 namespace Platform::Threading::Tests
 {
-    TEST(Sync, Basic)
+    TEST(Synchronization, Sync)
     {
         using namespace Synchronization;
 
@@ -28,6 +28,39 @@ namespace Platform::Threading::Tests
         }
 
         for (auto&& [key, value] : Drop(std::move(dict))) {
+            ASSERT_EQ(value, threads_count);
+        }
+    }
+
+    TEST(Synchronization, Explicit)
+    {
+        using namespace Synchronization;
+
+        auto threads_count = 1000;
+        auto map_operations_count = 100;
+
+        std::map<std::string, int> dict{};
+
+        auto work = [&]
+        {
+            for (int i = 0; i < map_operations_count; i++)
+            {
+                dict.operator[](std::to_string(i))++;
+            }
+        };
+
+        std::vector<std::thread> threads;
+
+        for (int i = 0; i < threads_count; i++)
+        {
+            threads.push_back(std::thread([&] { ExecuteWriteOperation(work); }));
+        }
+
+        for (auto& thread : threads) {
+            thread.join();
+        }
+
+        for (auto&& [key, value] : dict) {
             ASSERT_EQ(value, threads_count);
         }
     }
